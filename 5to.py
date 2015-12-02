029d177668f8549dab855af253531ca61932c168
kunstschmiede <hsburgess@gmail.com>
2015-12-02 11:39:05 -0800

    through exercise 17

--- 5to.py
+++ 5to.py
@@ -115,26 +115,88 @@
 
 
 # Exercise 14
-from sys import argv
+# from sys import argv
 
-script, user_name = argv
-prompt = '< '
+# script, user_name = argv
+# prompt = '< '
 
-print "Hi %s, I'm the %s script." % (user_name, script)
-print "I'd like to ask you a few questions."
-print "Do you like me %s?" % user_name
-likes = raw_input(prompt)
+# print "Hi %s, I'm the %s script." % (user_name, script)
+# print "I'd like to ask you a few questions."
+# print "Do you like me %s?" % user_name
+# likes = raw_input(prompt)
 
-print "Where do you live %s?" % user_name
-lives = raw_input(prompt)
+# print "Where do you live %s?" % user_name
+# lives = raw_input(prompt)
 
-print "What kind of computer do you have?"
-computer = raw_input(prompt)
+# print "What kind of computer do you have?"
+# computer = raw_input(prompt)
 
-print """
-Alright, so you said %r about liking me.
-You live in %r.  Not sure where that is.
-And you have a %r computer.  Nice.
-""" % (likes, lives, computer)
+# print """
+# Alright, so you said %r about liking me.
+# You live in %r.  Not sure where that is.
+# And you have a %r computer.  Nice.
+# """ % (likes, lives, computer)
+
+
+# Exercise 15
+# from sys import argv
+
+# script, filename = argv
+
+# txt = open(filename)
+
+# print "Here's your file %r:" % filename
+# print txt.read()
+
+# print "Type the filename again:"
+# file_again = raw_input("> ")
+
+# txt_again = open(file_again)
+
+# print txt_again.read()
+
+# txt.close()
+# txt_again.close()
+
+
+# Exercise 16
+# from sys import argv
+
+# script, filename = argv
+
+# print "We're going to erase %r." % filename
+# print "If you don't want that, hit CTRL-C (^C)."
+# print "If you do want that, hit RETURN."
+
+# raw_input("?")
+
+# print "Opening the file..."
+# target = open(filename, 'w+')
+
+# print "Truncating the file.  Goodbye!"
+# target.truncate()
+
+# print "Now I'm going to ask you for three lines."
+
+# line1 = raw_input("line 1: ")
+# line2 = raw_input("line 2: ")
+# line3 = raw_input("line 3: ")
+
+# print "I'm going to write these to the file."
+
+# target.write(line1)
+# target.write("\n")
+# target.write(line2)
+# target.write("\n")
+# target.write(line3)
+# target.write("\n")
+
+# print target.read()
+
+# print "And finally, we close it."
+# target.close()
+
+
+# Exercise 17
 
 
